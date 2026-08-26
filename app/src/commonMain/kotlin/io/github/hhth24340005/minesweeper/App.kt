91a5ee59345917135a4027374de6f6da2ad6e8b8
Tatsuya Mukai <hhth24340005@class.hht.ac.jp>
2026-08-26 16:22:54 +0900

    App()からGame()にゲーム本体を移動させてあとあとのために準備

--- app/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/App.kt
+++ app/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/App.kt
@@ -1,36 +1,10 @@
 package io.github.hhth24340005.minesweeper
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 public fun App() {
-  Column(
-    modifier = Modifier.fillMaxSize(),
-    horizontalAlignment = Alignment.CenterHorizontally,
-    verticalArrangement = Arrangement.Center,
-  ) {
-    Row {
-      ConcealedCell(isMarked = false)
-      ConcealedCell(isMarked = false)
-      ConcealedCell(isMarked = false)
-    }
-    Row {
-      RevealedEmptyCell(numberOfMinesAround = 0)
-      RevealedEmptyCell(numberOfMinesAround = 2)
-      ConcealedCell(isMarked = true)
-    }
-    Row {
-      ConcealedCell(isMarked = false)
-      ConcealedCell(isMarked = false)
-      RevealedMineCell()
-    }
-  }
+  Game()
 }
---
+++ app/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/Game.kt
@@ -0,0 +1,34 @@
+package io.github.hhth24340005.minesweeper
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+public fun Game() {
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center,
+  ) {
+    Row {
+      ConcealedCell(isMarked = false)
+      ConcealedCell(isMarked = false)
+      ConcealedCell(isMarked = false)
+    }
+    Row {
+      RevealedEmptyCell(numberOfMinesAround = 0)
+      RevealedEmptyCell(numberOfMinesAround = 2)
+      ConcealedCell(isMarked = true)
+    }
+    Row {
+      ConcealedCell(isMarked = false)
+      ConcealedCell(isMarked = false)
+      RevealedMineCell()
+    }
+  }
+}
