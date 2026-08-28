d9f80cd50d0b1c6fd8fafcc368e727bf4cffec6a
Tatsuya Mukai <hhth24340005@class.hht.ac.jp>
2026-08-28 11:12:25 +0900

    Matrix<T>を用意して座標計算を分離

--- app/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/logic/Stage.kt
+++ app/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/logic/Stage.kt
@@ -4,16 +4,44 @@ import kotlin.collections.contains
 import kotlin.math.ceil
 import kotlin.random.Random
 
+public interface Matrix<T> {
+  public val rows: List<List<T>>
+
+  public fun rowOf(
+    cell: T,
+  ): Int =
+    rows.indexOfFirst { it.contains(cell) }
+
+  public fun columnOf(
+    cell: T,
+  ): Int =
+    rows[rowOf(cell)].indexOf(cell)
+
+  public fun adjacentCellsOf(
+    cell: T,
+  ): Set<T> =
+    (-1..1)
+      .flatMap { x ->
+        (-1..1).mapNotNull { y ->
+          if (x == 0 && y == 0) {
+            return@mapNotNull null
+          }
+          val row = rows.getOrNull(rowOf(cell) + x)
+          row?.getOrNull(columnOf(cell) + y)
+        }
+      }.toSet()
+}
+
 public class Stage(
   width: Int,
   height: Int,
   private val mineDensity: Double,
   private val random: Random = Random,
-) {
+) : Matrix<Stage.CellId> {
   private var isInitialized: Boolean = false
   private val mines: MutableSet<CellId> = mutableSetOf()
 
-  public val rows: List<List<CellId>> =
+  public override val rows: List<List<CellId>> =
     List(height) { List(width) { CellId() } }
 
   init {
@@ -37,14 +65,13 @@ public class Stage(
       cell: CellId,
       map: MutableMap<CellId, Int>,
     ) {
-      val minesAround = cell.adjacentCells.count { it in mines }
-
+      val minesAround = adjacentCellsOf(cell).count { it in mines }
       map[cell] = minesAround
       if (minesAround != 0) {
         return
       }
 
-      cell.adjacentCells
+      adjacentCellsOf(cell)
         .filter { it !in map }
         .forEach {
           revealInner(it, map)
@@ -69,30 +96,9 @@ public class Stage(
   ) {
     val candidates = rows.flatten().toMutableSet()
     candidates -= startingCell
-    candidates -= startingCell.adjacentCells
+    candidates -= adjacentCellsOf(startingCell)
     val minesCount = ceil(mineDensity * candidates.size).toInt()
     mines.addAll(candidates.shuffled(random).take(minesCount))
   }
-
-  private val CellId.adjacentCells: Set<CellId> get() =
-    ((-1)..1)
-      .flatMap { x ->
-        ((-1)..1).mapNotNull { y ->
-          if (x == 0 && y == 0) {
-            return@mapNotNull null
-          }
-          val (currentX, currentY) = position
-          rows
-            .getOrNull(currentY + y)
-            ?.getOrNull(currentX + x)
-        }
-      }.toSet()
-
-
-  private val CellId.position: Pair<Int, Int> get() {
-    val row = rows.first { it.contains(this) }
-    val y = rows.indexOf(row)
-    val x = row.indexOf(this)
-    return x to y
-  }
 }
+
