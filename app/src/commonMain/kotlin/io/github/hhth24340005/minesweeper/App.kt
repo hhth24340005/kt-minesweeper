3b54cf9b611eb07877e99175e65fb257de615972
Tatsuya Mukai <hhth24340005@class.hht.ac.jp>
2026-08-31 02:09:43 +0900

    ゲームループが完成した

--- app/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/App.kt
+++ app/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/App.kt
@@ -1,6 +1,10 @@
 package io.github.hhth24340005.minesweeper
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.hhth24340005.minesweeper.logic.MinesweeperStage
 import io.github.hhth24340005.minesweeper.logic.hexGridOf
@@ -8,14 +12,23 @@ import io.github.hhth24340005.minesweeper.logic.hexGridOf
 @Composable
 @Preview
 public fun App() {
-  Game(
-    gridComposer = GridComposer.hexOf(),
-    uninitializedStage =
-      MinesweeperStage.prepare(
-        width = 9,
-        height = 9,
-        gridFactory = ::hexGridOf,
-        mineDensity = 0.2,
-      ),
-  )
+  var stage by mutableStateOf(stageOf())
+
+  val deferred =
+    Game(
+      gridComposer = GridComposer.hexOf(),
+      uninitializedStage = stage,
+    )
+  LaunchedEffect(deferred) {
+    deferred.await()
+    stage = stageOf()
+  }
 }
+
+private fun stageOf(): MinesweeperStage.Uninitialized =
+  MinesweeperStage.prepare(
+    width = 9,
+    height = 9,
+    gridFactory = ::hexGridOf,
+    mineDensity = 0.2,
+  )
--- app/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/Game.kt
+++ app/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/Game.kt
@@ -1,16 +1,36 @@
 package io.github.hhth24340005.minesweeper
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.PointerButton
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.em
 import io.github.hhth24340005.minesweeper.logic.MinesweeperStage
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.selects.select
 
 @Composable
 public fun Game(
   gridComposer: GridComposer,
   uninitializedStage: MinesweeperStage.Uninitialized,
-) {
+): Deferred<GameResult> {
+  val deferred =
+    remember(uninitializedStage) {
+      CompletableDeferred<GameResult>()
+    }
   LaunchedRenderer(uninitializedStage) {
     layerOf().use { layer0 ->
       val clickedCell =
@@ -22,23 +42,83 @@ public fun Game(
           }
         }
       val stage = uninitializedStage.initialize(clickedCell)
-      layer0<Nothing> {
-        val clicks = gridComposer.Grid(stage.rows)
-        LaunchedEffect(clicks) {
-          while (true) {
-            val (click, cell) = clicks.firstOrNull() ?: break
-            when (click) {
-              PointerButton.Primary -> {
-                stage.reveal(cell)
-              }
+      val result =
+        layer0 { completeGame ->
+          Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+          ) {
+            val clicks = gridComposer.Grid(stage.rows)
+            LaunchedRenderer(clicks) {
+              val parentJob = Job(coroutineContext[Job])
+              val coroutine = CoroutineScope(coroutineContext + parentJob)
+              val clickJob =
+                coroutine.launch {
+                  while (true) {
+                    val (click, cell) = clicks.firstOrNull() ?: break
+                    when (click) {
+                      PointerButton.Primary -> {
+                        stage.reveal(cell)
+                      }
 
-              PointerButton.Secondary -> {
-                stage.toggleMark(cell)
-              }
+                      PointerButton.Secondary -> {
+                        stage.toggleMark(cell)
+                      }
+                    }
+                  }
+                }
+              val winJob =
+                coroutine.launch {
+                  stage.awaitWin()
+                  clickJob.cancel()
+                  layerOf().use { layer1 ->
+                    layer1 { proceed ->
+                      Text(
+                        "You win!",
+                        modifier =
+                          Modifier.clickable { proceed(Unit) },
+                        fontSize = 4.em,
+                        textDecoration = TextDecoration.Underline,
+                      )
+                    }
+                  }
+                }
+              val loseJob =
+                coroutine.launch {
+                  stage.awaitLose()
+                  clickJob.cancel()
+                  layerOf().use { layer1 ->
+                    layer1 { proceed ->
+                      Text(
+                        "You lose!",
+                        modifier =
+                          Modifier.clickable { proceed(Unit) },
+                        fontSize = 4.em,
+                        textDecoration = TextDecoration.Underline,
+                      )
+                    }
+                  }
+                }
+              val result =
+                select {
+                  winJob.onJoin { GameResult.Win }
+                  loseJob.onJoin { GameResult.Lose }
+                }
+              parentJob.cancelAndJoin()
+              completeGame(result)
             }
           }
         }
-      }
+      deferred.complete(result)
     }
   }
+  return deferred
+}
+
+public sealed interface GameResult {
+  public data object Win : GameResult
+
+  public data object Lose : GameResult
+
+  public data object Canceled : GameResult
 }
