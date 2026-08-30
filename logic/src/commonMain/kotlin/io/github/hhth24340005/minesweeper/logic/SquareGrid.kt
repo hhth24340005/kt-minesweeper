797425e3afa28eda1c98d9468e4edf226f89d870
Tatsuya Mukai <hhth24340005@class.hht.ac.jp>
2026-08-30 13:17:57 +0900

    SquareGridの細かいところ修正

--- logic/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/logic/SquareGrid.kt
+++ logic/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/logic/SquareGrid.kt
@@ -3,19 +3,26 @@ package io.github.hhth24340005.minesweeper.logic
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
-public class SquareGrid<T>(
+public class SquareGrid<T> private constructor(
   public override val rows: ImmutableList<ImmutableList<T>>,
 ) : Grid<T> {
+  public val width: Int get() = rows.first().size
+  public val height: Int get() = rows.size
+
   public constructor(
     width: Int,
     height: Int,
     init: (x: Int, y: Int) -> T,
   ) : this(
-    List(height) { y ->
-      List(width) { x ->
-        init(x, y)
+    run {
+      require(0 < width)
+      require(0 < height)
+      List(height) { y ->
+        List(width) { x ->
+          init(x, y)
+        }.toImmutableList()
       }.toImmutableList()
-    }.toImmutableList(),
+    },
   )
 
   public constructor(
@@ -43,24 +50,47 @@ public class SquareGrid<T>(
   override fun rowOf(
     cell: T,
   ): Int =
-    cellToPos[cell]!!.second
+    cellToPos[cell]?.second
+      ?: throw IllegalArgumentException(
+        "This ($this) is not the owner for the cell ($cell)",
+      )
 
   override fun columnOf(
     cell: T,
   ): Int =
-    cellToPos[cell]!!.first
+    cellToPos[cell]?.first
+      ?: throw IllegalArgumentException(
+        "This ($this) is not the owner for the cell ($cell)",
+      )
 
   override fun adjacentCellsOf(
     cell: T,
-  ): Set<T> =
-    (-1..1)
-      .flatMap { x ->
-        (-1..1).mapNotNull { y ->
-          if (x == 0 && y == 0) {
+  ): Set<T> {
+    val y = rowOf(cell)
+    val x = columnOf(cell)
+    return (-1..1)
+      .flatMap { yOffset ->
+        (-1..1).mapNotNull { xOffset ->
+          if (xOffset == 0 && yOffset == 0) {
             return@mapNotNull null
           }
-          val row = rows.getOrNull(rowOf(cell) + x)
-          row?.getOrNull(columnOf(cell) + y)
+          val row = rows.getOrNull(y + yOffset)
+          row?.getOrNull(x + xOffset)
         }
       }.toSet()
+  }
+
+  public override fun toString(): String =
+    "SquareGrid($width x $height)@${hashCode()}"
+
+  public override fun equals(
+    other: Any?,
+  ): Boolean {
+    if (this === other) return true
+    if (other !is SquareGrid<*>) return false
+    return rows == other.rows
+  }
+
+  public override fun hashCode(): Int =
+    rows.hashCode()
 }
