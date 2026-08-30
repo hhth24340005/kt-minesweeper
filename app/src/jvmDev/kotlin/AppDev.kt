6a442b9ef5457be5db9723ea89642ab7b1b586b5
Tatsuya Mukai <hhth24340005@class.hht.ac.jp>
2026-08-31 03:35:50 +0900

    いらない謎のjvmDevを削除

--- app/src/jvmDev/kotlin/AppDev.kt
+++
@@ -1,9 +0,0 @@
-import androidx.compose.runtime.Composable
-import io.github.hhth24340005.minesweeper.App
-import org.jetbrains.compose.reload.DevelopmentEntryPoint
-
-@DevelopmentEntryPoint(windowWidth = 480, windowHeight = 640)
-@Composable
-public fun AppDev() {
-  App()
-}
