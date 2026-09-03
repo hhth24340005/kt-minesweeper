7573f629db1692681ade2d733e98ca586ea06b08
Tatsuya Mukai <hhth24340005@class.hht.ac.jp>
2026-09-03 23:48:52 +0900

    selectに代わるraceを作った

---
+++ lib/src/commonMain/kotlin/io/github/hhth24340005/minesweeper/Race.kt
@@ -0,0 +1,74 @@
+package io.github.hhth24340005.minesweeper
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.yield
+import kotlin.coroutines.cancellation.CancellationException
+
+public suspend fun <R> race(
+  build: RaceBuilder<R>.() -> Unit,
+): R {
+  val onWin =
+    coroutineScope coroutine@{
+      val racers =
+        buildList jobs@{
+          RaceBuilderImpl(this@coroutine, this@jobs).build()
+        }.merge()
+      racers.firstOrNull() ?: error("No racer was registered")
+    }
+  yield()
+  return onWin()
+}
+
+public interface RaceBuilder<R> : CoroutineScope {
+  public fun racer(
+    block:
+      suspend CoroutineScope.() -> (suspend () -> R),
+  )
+
+  public fun whileActive(
+    block: suspend CoroutineScope.() -> Nothing,
+  ) {
+    racer {
+      block()
+    }
+  }
+
+  public fun Job.onJoin(
+    onWin: suspend () -> R,
+  ) {
+    racer {
+      try {
+        join()
+        onWin
+      } catch (e: CancellationException) {
+        cancel(e)
+        throw e
+      }
+    }
+  }
+}
+
+@PublishedApi
+internal class RaceBuilderImpl<R>(
+  private val coroutine: CoroutineScope,
+  private val jobs: MutableList<
+    Flow<suspend () -> R>,
+  >,
+) : RaceBuilder<R>,
+  CoroutineScope by coroutine {
+  override fun racer(
+    block:
+      suspend CoroutineScope.() -> (suspend () -> R),
+  ) {
+    jobs +=
+      flow {
+        emit(coroutine.block())
+      }
+  }
+}
